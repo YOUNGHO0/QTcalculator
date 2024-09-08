7b629c9a9fc1dd62526ef68d40ebb9c9ffa3d3b3
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-08 16:50:43 +0900

    Feat: Expression 표현식 생성

---
+++ PostFixExpression.cpp
@@ -0,0 +1,7 @@
+//
+// Created by lee on 2024. 8. 8..
+//
+
+#include "PostFixExpression.h"
+
+
---
+++ PostFixExpression.h
@@ -0,0 +1,18 @@
+//
+// Created by lee on 2024. 8. 8..
+//
+
+#ifndef POSTFIXEXPRESSION_H
+#define POSTFIXEXPRESSION_H
+
+
+
+class PostFixExpression {
+
+   public:
+   virtual int process() =0;
+};
+
+
+
+#endif //POSTFIXEXPRESSION_H
