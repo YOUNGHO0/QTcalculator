3314fb555cf7e517ad10f7a0399496b4fc383d3d
YOUNGHO0 <31pqopq13@gmail.com>
2024-09-08 16:49:29 +0900

    Feat: 프로젝트 시작

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QPushButton>
+
+#include "Widget.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    Widget w;
+    w.show();
+    return QApplication::exec();
+}
